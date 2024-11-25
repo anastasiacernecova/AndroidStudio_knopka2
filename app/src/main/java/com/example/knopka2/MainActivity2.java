837f80cda6693e62a12a1be6b175f1fbfd7390dc
anastasiacernecova <ChernezovaAsja666@yandex.ru>
2024-11-25 10:22:07 +0300

    2 лаба изменено

--- app/src/main/java/com/example/knopka2/MainActivity2.java
+++ app/src/main/java/com/example/knopka2/MainActivity2.java
@@ -3,8 +3,6 @@ package com.example.knopka2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,31 +10,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    private TextView helloText;
-    private ImageButton imageButton;
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        helloText = (TextView) findViewById(R.id.helloText);
-        imageButton = (ImageButton) findViewById(R.id.imageButton);
-    }
-
-    public void onStartButtonClick(View view) {
-        imageButton.setImageResource(R.drawable.android);
-        helloText.setVisibility(View.VISIBLE);
-        imageButton.setClickable(false);
     }
     public void Click(View view) {
-        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        Intent intent = new Intent(MainActivity2.this, MainActivity.class);
         startActivity(intent);
         finish();
     }
